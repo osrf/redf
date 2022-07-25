0144a204c2c5b04b4790f29ea0fdb972c2622e95
Teo Koon Peng <koonpeng@openrobotics.org>
2022-07-25 15:30:57 +0800

    switch to tera (#5)
    tera is an alternative template engine to handlebars, it follows the jinja2 syntax and allows for much more powerful features in the template. This allows us to write more complex control structures within the template, greatly simplifying the "pre-processing" needed in rust code.
    
    Signed-off-by: Teo Koon Peng <koonpeng@openrobotics.org>

--- src/cpp/mod.rs
+++ src/cpp/mod.rs
@@ -1,129 +1,30 @@
 use crate::helpers::register_helpers;
-use crate::redf::{
-    DurabilityPolicy, Endpoint, LivelinessPolicy, Qos, QosPreset, QosProfile, Redf,
-    ReliabilityPolicy,
-};
+use crate::redf::{Endpoint, Redf};
 use convert_case::{Case, Casing};
-use handlebars::Handlebars;
 use serde::Serialize;
 use std::collections::BTreeSet;
 use std::error::Error;
 use std::path::Path;
-
-fn qos_profile(profile: QosProfile) -> String {
-    let reliability = match profile.reliability {
-        Some(ReliabilityPolicy::BestEffort) => "rclcpp::ReliabilityPolicy::BestEffort",
-        Some(ReliabilityPolicy::Reliable) => "rclcpp::ReliabilityPolicy::Reliable",
-        _ => "rclcpp::ReliabilityPolicy::SystemDefault",
-    };
-    let durability = match profile.durability {
-        Some(DurabilityPolicy::TransientLocal) => "rclcpp::DurabilityPolicy::TransientLocal",
-        Some(DurabilityPolicy::Volatile) => "rclcpp::DurabilityPolicy::Volatile",
-        _ => "rclcpp::DurabilityPolicy::SystemDefault",
-    };
-    let liveliness = match profile.liveliness {
-        Some(LivelinessPolicy::Automatic) => "rclcpp::LivelinessPolicy::Automatic",
-        Some(LivelinessPolicy::ManualByTopic) => "rclcpp::LivelinessPolicy::ManualByTopic",
-        _ => "rclcpp::LivelinessPolicy::SystemDefault",
-    };
-    let deadline = match profile.deadline {
-        Some(d) => format!("rclcpp::Duration{{{}, {}}}", d.secs, d.nanosecs),
-        None => "".to_string(),
-    };
-    let lifespan = match profile.lifespan {
-        Some(d) => format!("rclcpp::Duration{{{}, {}}}", d.secs, d.nanosecs),
-        None => "".to_string(),
-    };
-    let lease_duration = match profile.lease_duration {
-        Some(d) => format!("rclcpp::Duration{{{}, {}}}", d.secs, d.nanosecs),
-        None => "".to_string(),
-    };
-    let avoid_ros_namespace_conventions = match profile.avoid_ros_namespace_conventions {
-        Some(b) => format!("{b}"),
-        None => "".to_string(),
-    };
-
-    format!(
-        r#"[history_policy, depth]() {{
-      rclcpp::QoS qos{{depth}};
-      qos.history(history_policy);
-      qos.reliability({reliability});
-      qos.durability({durability});
-      qos.liveliness({liveliness});
-      qos.deadline({deadline});
-      qos.lifespan({lifespan});
-      qos.liveliness_lease_duration({lease_duration});
-      qos.avoid_ros_namespace_conventions({avoid_ros_namespace_conventions});
-      return qos;
-    }}()"#
-    )
-}
-
-fn qos_preset(preset: QosPreset) -> String {
-    match preset {
-        QosPreset::Clock => "rclcpp::ClockQoS{}".to_string(),
-        QosPreset::ParameterEvents => "rclcpp::ParameterEventsQoS{}".to_string(),
-        QosPreset::Parameters => "rclcpp::ParametersQoS{}".to_string(),
-        QosPreset::Rosout => "rclcpp::RosoutQoS{}".to_string(),
-        QosPreset::SensorData => "rclcpp::SensorDataQoS{}".to_string(),
-        QosPreset::Services => "rclcpp::ServicesQoS{}".to_string(),
-        QosPreset::SystemDefaults => "rclcpp::SystemDefaultsQoS{}".to_string(),
-    }
-}
-
-fn qos_helper(
-    h: &handlebars::Helper,
-    _: &Handlebars,
-    _: &handlebars::Context,
-    _: &mut handlebars::RenderContext,
-    out: &mut dyn handlebars::Output,
-) -> Result<(), handlebars::RenderError> {
-    let qos: Option<Qos> = serde_json::from_value(h.param(0).unwrap().value().clone())?;
-    let rendered = match qos {
-        Some(Qos::Profile(profile)) => qos_profile(profile),
-        Some(Qos::Preset(preset)) => qos_preset(preset),
-        _ => format!(
-            r#"[history_policy, depth]() {{
-      rclcpp::QoS qos{{depth}};
-      qos.history(history_policy);
-      return qos;
-    }}()"#
-        ),
-    };
-    out.write(&rendered)?;
-    Ok(())
-}
-
-fn cpp_type_helper(
-    h: &handlebars::Helper,
-    _: &Handlebars,
-    _: &handlebars::Context,
-    _: &mut handlebars::RenderContext,
-    out: &mut dyn handlebars::Output,
-) -> Result<(), handlebars::RenderError> {
-    let msg_type = h.param(0).unwrap().render();
-    out.write(&msg_type.replace("/", "::"))?;
-    Ok(())
-}
+use tera::Tera;
 
 #[derive(Serialize)]
 struct Context<'a> {
     redf: &'a Redf,
     project_name: String,
     packages: Vec<String>,
+    messages: BTreeSet<String>,
     namespace: String,
     includes: Vec<String>,
 }
 
 pub fn generate(redf: &Redf, outdir: &Path) -> Result<(), Box<dyn Error>> {
-    let mut hbs = Handlebars::new();
-    register_helpers(&mut hbs);
-    hbs.register_helper("cpp_type", Box::new(cpp_type_helper));
-    hbs.register_helper("qos", Box::new(qos_helper));
-    hbs.register_template_string("package.xml", include_str!("package.xml.tmpl"))?;
-    hbs.register_template_string("CMakeLists.txt", include_str!("CMakeLists.txt.tmpl"))?;
-    hbs.register_template_string("header", include_str!("header.hpp.tmpl"))?;
+    let mut tera = Tera::default();
+    register_helpers(&mut tera);
+    tera.add_raw_template("package.xml", include_str!("package.xml.j2"))?;
+    tera.add_raw_template("CMakeLists.txt", include_str!("CMakeLists.txt.j2"))?;
+    tera.add_raw_template("header.hpp", include_str!("header.hpp.j2"))?;
 
+    let project_name = redf.title.to_case(Case::Snake).clone();
     let messages: BTreeSet<String> = redf
         .endpoints
         .iter()
@@ -147,20 +48,21 @@ pub fn generate(redf: &Redf, outdir: &Path) -> Result<(), Box<dyn Error>> {
         .map(|msg| format!("#include \"{}.hpp\"", msg))
         .collect();
 
-    let ctx = Context {
+    let ctx = tera::Context::from_serialize(Context {
         redf,
-        project_name: redf.title.to_case(Case::Snake).clone(),
+        project_name: project_name.clone(),
         packages,
+        messages,
         namespace,
         includes,
-    };
+    })?;
 
     let f = std::fs::File::create(outdir.join("package.xml"))?;
-    hbs.render_to_write("package.xml", &ctx, f)?;
+    tera.render_to("package.xml", &ctx, f)?;
     let f = std::fs::File::create(outdir.join("CMakeLists.txt"))?;
-    hbs.render_to_write("CMakeLists.txt", &ctx, f)?;
-    let f = std::fs::File::create(outdir.join(format!("{}.hpp", &ctx.project_name)))?;
-    hbs.render_to_write("header", &ctx, f)?;
+    tera.render_to("CMakeLists.txt", &ctx, f)?;
+    let f = std::fs::File::create(outdir.join(format!("{}.hpp", &project_name)))?;
+    tera.render_to("header.hpp", &ctx, f)?;
 
     Ok(())
 }
