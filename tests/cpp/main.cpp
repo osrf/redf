4500cc01823307e90debc866f3b65b127c99b506
Teo Koon Peng <koonpeng@openrobotics.org>
2022-07-29 16:17:26 +0800

    formatting
    Signed-off-by: Teo Koon Peng <koonpeng@openrobotics.org>

--- tests/cpp/main.cpp
+++ tests/cpp/main.cpp
@@ -6,13 +6,15 @@
 #include <rclcpp/rclcpp.hpp>
 #include <rclcpp_action/rclcpp_action.hpp>
 
-int main(int argc, char* argv[]) {
+int main(int argc, char * argv[])
+{
   rclcpp::init(argc, argv);
   auto test_node = rclcpp::Node::make_shared("test_node");
 
   // publish
   using test_namespace::test_api::SimpleQoS;
-  auto pub = test_node->create_publisher<SimpleQoS::MessageType>(SimpleQoS::topic_name(), SimpleQoS::qos());
+  auto pub = test_node->create_publisher<SimpleQoS::MessageType>(
+    SimpleQoS::topic_name(), SimpleQoS::qos());
   SimpleQoS::MessageType msg;
   msg.data = "hello";
   pub->publish(msg);
@@ -31,8 +33,10 @@ int main(int argc, char* argv[]) {
 
   // server
   auto server = test_node->create_service<TestService::ServiceType>(
-    TestService::service_name(), [](std::shared_ptr<rmw_request_id_t>, TestService::ServiceType::Request::SharedPtr, TestService::ServiceType::Response::SharedPtr) {});
-  
+    TestService::service_name(),
+    [](std::shared_ptr<rmw_request_id_t>, TestService::ServiceType::Request::SharedPtr,
+    TestService::ServiceType::Response::SharedPtr) {});
+
   // action client
   using test_namespace::test_api::TestAction;
   auto action_client = rclcpp_action::create_client<TestAction::ActionType>(
@@ -43,8 +47,12 @@ int main(int argc, char* argv[]) {
   // action server
   auto action_server = rclcpp_action::create_server<TestAction::ActionType>(
     test_node, TestAction::action_name(),
-    [](const rclcpp_action::GoalUUID&, TestAction::ActionType::Goal::ConstSharedPtr) { return rclcpp_action::GoalResponse::ACCEPT_AND_EXECUTE; },
-    [](std::shared_ptr<rclcpp_action::ServerGoalHandle<TestAction::ActionType>>) { return rclcpp_action::CancelResponse::ACCEPT; },
+    [](const rclcpp_action::GoalUUID &, TestAction::ActionType::Goal::ConstSharedPtr) {
+      return rclcpp_action::GoalResponse::ACCEPT_AND_EXECUTE;
+    },
+    [](std::shared_ptr<rclcpp_action::ServerGoalHandle<TestAction::ActionType>>) {
+      return rclcpp_action::CancelResponse::ACCEPT;
+    },
     [](std::shared_ptr<rclcpp_action::ServerGoalHandle<TestAction::ActionType>>) {}
   );
 
